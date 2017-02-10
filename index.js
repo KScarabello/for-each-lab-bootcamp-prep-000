412301d3852f31388de2dc56e4722b02e8d942a0
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2017-02-10 04:57:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+array = []
+
+function iterativeLog(array){
+  array.forEach(imTired)
+}
+
+function imTired (element, index){
+  console.log(`${index}: ${element}`)
+}
+
+function iterate(callback){
+  var array = [1, 2, 3]
+  array.forEach(callback)
+  return array
+
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
